a2e4caeb8f2993156e3c451e3083ab06891f7319
Shrikant023 <ramayanagita023@gmail.com>
2024-10-04 17:58:10 +0530

    more numbers included

--- scripts.js
+++ scripts.js
@@ -17,15 +17,17 @@ function changeQuantity(id, value) {
 function calculateTotal() {
     let qty100 = parseInt(document.getElementById('qty100').value) || 0;
     let qty400 = parseInt(document.getElementById('qty400').value) || 0;
+    let qty500 = parseInt(document.getElementById('qty500').value) || 0;
     let qty700 = parseInt(document.getElementById('qty700').value) || 0;
     let qty900 = parseInt(document.getElementById('qty900').value) || 0;
+    let qty1000 = parseInt(document.getElementById('qty1000').value) || 0;
     let qty1300 = parseInt(document.getElementById('qty1300').value) || 0;
     let qty10000 = parseInt(document.getElementById('qty10000').value) || 0;
     let qty15000 = parseInt(document.getElementById('qty15000').value) || 0;
 
     // Multiply quantities by their respective prices
-    let total = (qty100 * 100) + (qty400 * 400) + (qty700 * 700) +
-        (qty900 * 900) + (qty1300 * 1300) + (qty10000 * 10000) +
+    let total = (qty100 * 100) + (qty400 * 400) + (qty500 * 500) + (qty700 * 700) +
+        (qty900 * 900) + (qty1000 * 1000) + (qty1300 * 1300) + (qty10000 * 10000) +
         (qty15000 * 15000);
 
     // Update the total amount displayed on the page
