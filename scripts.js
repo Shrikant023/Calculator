2471f0aecc254c96bc007a5e6c5df90914edf379
Shrikant023 <ramayanagita023@gmail.com>
2024-10-08 12:47:55 +0530

    added dark mode

--- scripts.js
+++ scripts.js
@@ -1,3 +1,8 @@
+// Function to toggle dark mode when the ProfitPulse heading is clicked
+function toggleDarkMode() {
+    document.body.classList.toggle('dark-mode');
+}
+
 // Function to change the quantity (increase or decrease)
 function changeQuantity(id, value) {
     let inputField = document.getElementById(id);
@@ -15,7 +20,6 @@ function changeQuantity(id, value) {
 
 // Function to calculate the total amount
 function calculateTotal() {
-    // Get the values for all quantities or default them to 0 if empty
     let qty100 = parseInt(document.getElementById('qty100').value) || 0;
     let qty200 = parseInt(document.getElementById('qty200').value) || 0;
     let qty300 = parseInt(document.getElementById('qty300').value) || 0;
@@ -31,6 +35,10 @@ function calculateTotal() {
     let qty2000 = parseInt(document.getElementById('qty2000').value) || 0;
     let qty3000 = parseInt(document.getElementById('qty3000').value) || 0;
     let qty5000 = parseInt(document.getElementById('qty5000').value) || 0;
+    let qty7000 = parseInt(document.getElementById('qty7000').value) || 0;
+    let qty9000 = parseInt(document.getElementById('qty9000').value) || 0;
+    let qty10000 = parseInt(document.getElementById('qty10000').value) || 0;
+    let qty15000 = parseInt(document.getElementById('qty15000').value) || 0;
     let qty500000 = parseInt(document.getElementById('qty500000').value) || 0;
 
     // Multiply quantities by their respective prices and calculate the total
@@ -39,7 +47,8 @@ function calculateTotal() {
         (qty800 * 800) + (qty900 * 900) + (qty1000 * 1000) +
         (qty1200 * 1200) + (qty1300 * 1300) + (qty1500 * 1500) +
         (qty2000 * 2000) + (qty3000 * 3000) + (qty5000 * 5000) +
-        (qty500000 * 500000);
+        (qty7000 * 7000) + (qty9000 * 9000) + (qty10000 * 10000) +
+        (qty15000 * 15000) + (qty500000 * 500000);
 
     // Update the total amount displayed on the page
     document.getElementById('totalAmount').innerHTML = total;
